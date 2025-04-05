f355a7449f5a202bbfddd5bb1f8bd18065f084ef
T. Tung Mai <tungous.tung@gmail.com>
2025-04-06 00:09:00 +0200

    <SpeedInsights />

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
         <div className="z-999 fixed bottom-2 right-2 lg:text-[10px] text-[8px] mix-blend-difference text-white font-bold tracking-tighter">
           © 2025 Seventyfour. All Rights Reserved.
         </div>
+        <SpeedInsights />
       </body>
     </html>
   );
